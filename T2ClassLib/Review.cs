c064369b7d6ed25123d0c849f88260fda0f83434
kjfaires <kjfaires@crimson.ua.edu>
2016-04-19 16:17:49 -0500

    Reward and Review Classes

---
+++ T2ClassLib/Review.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2ClassLib
+{
+    class Review
+    {
+        //members
+        private int reviewId;
+        private string comment;
+        private int starRating;
+
+        //constructors
+        public Review()
+        {
+            reviewId = 876345;
+            comment = " ";
+            starRating = 4;
+        }
+
+        //properties
+
+        public int ReviewId
+        {
+            get {return reviewId; }
+            set { ReviewId = value; }
+        }
+
+        public string Comment
+        {
+            get { return comment; }
+            set { Comment = value; }
+        }
+
+        public int StarRating
+        {
+            get { return starRating; }
+            set { StarRating = value; }
+        }
+    }
+}
---
+++ T2ClassLib/Reward.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2ClassLib
+{
+    class Reward
+    {
+        //members
+        private int rewardId;
+        private double discountRate;
+        private DateTime expDate;
+
+        //constructors
+        public Reward()
+        {
+            rewardId = 67876;
+            discountRate = .15;
+            expDate = DateTime.Now;
+        }
+
+        //properties
+
+        public int RewardId
+        {
+            get { return rewardId; }
+            set { RewardId = value; }
+        }
+
+        public double DiscountRate
+        {
+            get { return discountRate; }
+            set { DiscountRate = value; }
+        }
+
+        public DateTime ExpDate
+        {
+            get { return expDate; }
+            set { ExpDate = value; }
+        }
+    }
+}
