5331aa61964e21614c7256febe47914e24245658
Mikayla Moles <mikayla.moles@gmail.com>
2016-04-19 17:47:43 -0500

    review form

--- 220ProjectHUB/Form1.Designer.cs
+++ 220ProjectHUB/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _220ProjectHUB
 {
-    partial class Form1
+    partial class Reviews_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,47 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.reviewsBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.movieToWishList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.starValueBar = new System.Windows.Forms.HScrollBar();
+            this.starValueLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // reviewsBox
+            // 
+            this.reviewsBox.FormattingEnabled = true;
+            this.reviewsBox.Location = new System.Drawing.Point(16, 34);
+            this.reviewsBox.Name = "reviewsBox";
+            this.reviewsBox.Size = new System.Drawing.Size(237, 173);
+            this.reviewsBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Reviews";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(179, 102);
+            this.button1.Location = new System.Drawing.Point(16, 248);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 24);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
+            // button2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(57, 70);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(97, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // movieToWishList
+            // 
+            this.movieToWishList.Location = new System.Drawing.Point(97, 217);
+            this.movieToWishList.Name = "movieToWishList";
+            this.movieToWishList.Size = new System.Drawing.Size(156, 20);
+            this.movieToWishList.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Review Name:";
+            // 
+            // starValueBar
+            // 
+            this.starValueBar.Location = new System.Drawing.Point(318, 220);
+            this.starValueBar.Maximum = 120;
+            this.starValueBar.Name = "starValueBar";
+            this.starValueBar.Size = new System.Drawing.Size(228, 18);
+            this.starValueBar.TabIndex = 8;
+            // 
+            // starValueLbl
+            // 
+            this.starValueLbl.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.starValueLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.starValueLbl.Location = new System.Drawing.Point(495, 184);
+            this.starValueLbl.Name = "starValueLbl";
+            this.starValueLbl.Size = new System.Drawing.Size(51, 23);
+            this.starValueLbl.TabIndex = 9;
+            this.starValueLbl.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(388, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(433, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(482, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(513, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(318, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 160);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(315, 14);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Comments:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(315, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Star Rating:";
+            // 
+            // Reviews_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.checkBox1);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(577, 283);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.starValueLbl);
+            this.Controls.Add(this.starValueBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.movieToWishList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reviewsBox);
+            this.Name = "Reviews_Form";
+            this.Text = "ReviewsForm";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.ListBox reviewsBox;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox movieToWishList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.HScrollBar starValueBar;
+        private System.Windows.Forms.Label starValueLbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- 220ProjectHUB/Form1.cs
+++ 220ProjectHUB/Form1.cs
@@ -10,11 +10,57 @@ using System.Windows.Forms;
 
 namespace _220ProjectHUB
 {
-    public partial class Form1 : Form
+    public partial class Reviews_Form : Form
     {
-        public Form1()
+        public Reviews_Form()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Item = movieToWishList.Text;
+            reviewsBox.Items.Add(Item);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reviewsBox.Items.RemoveAt(reviewsBox.SelectedIndex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            reviewsBox.Items.Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (starValueBar.Value < 20)
+            {
+                starValueLbl.Text = "1";
+            }
+            else if ((starValueBar.Value >= 40) && (starValueBar.Value < 60))
+            {
+                starValueLbl.Text = "2";
+            }
+            else if ((starValueBar.Value >= 60) && (starValueBar.Value < 80))
+            {
+                starValueLbl.Text = "3";
+            }
+            else if ((starValueBar.Value >= 80) && (starValueBar.Value < 100))
+            {
+                starValueLbl.Text = "4";
+            }
+            else if (starValueBar.Value >= 100)
+            {
+                starValueLbl.Text = "5";
+            }
         }
     }
 }
