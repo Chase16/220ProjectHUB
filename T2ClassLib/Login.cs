54ad78fcda0fe541e3357f030706933fbe88ff89
kjfaires <kjfaires@crimson.ua.edu>
2016-04-19 14:28:34 -0500

    Login Class Added (Made Password and IsAdmin properties private for security)

--- T2ClassLib/Login.cs
+++ T2ClassLib/Login.cs
@@ -8,6 +8,53 @@ namespace T2ClassLib
 {
     class Login
     {
+        //members
+        private int userId;
+        private string userName;
+        private string passWord;
+        private bool isAdmin;
+        private bool newMem;
+        
+        //constructors
+        public Login()
+        {
+            userId = 77;
+            userName = "johnsmith";
+            passWord = "RollTide15";
+            isAdmin = true;
+            newMem = false;
+        } 
 
+
+        //Encapsulation
+        public int UserID
+        {
+            get { return userId; }
+            set { UserID = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { UserName = value; }
+        }
+
+        protected string PassWord
+        {
+            get { return passWord; }
+            set { PassWord = value; }
+        }
+
+        protected bool IsAdmin
+        {
+            get { return isAdmin; }
+            set { IsAdmin = value; }
+        }
+
+        public bool NewMem
+        {
+            get { return newMem; }
+            set { NewMem = value; }
+        }
     }
 }
