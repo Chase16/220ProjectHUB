62a62cf3414ec72c1958eeb664f27be3b79528e8
kjfaires <kjfaires@crimson.ua.edu>
2016-04-19 19:10:15 -0500

    Corrected Transaction Class

--- T2ClassLib/Transactions.cs
+++ T2ClassLib/Transactions.cs
@@ -19,8 +19,33 @@ namespace T2ClassLib
         {
             transId = 7254;
             rentDate = DateTime.Now;
-           
-             
+            dueDate = DateTime.Now;
+            totalPrice = 10.00;
+        }
+
+        //properties
+        public int TransId
+        {
+            get { return transId; }
+            set { TransId = value; }
+        }
+
+        public DateTime RentDate
+        {
+            get { return rentDate; }
+            set { RentDate = value; }
+        }
+
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set { DueDate = value; }
+        }
+
+        public double TotalPrice
+        {
+            get { return totalPrice; }
+            set { TotalPrice = value; }
         }
     }
 }
